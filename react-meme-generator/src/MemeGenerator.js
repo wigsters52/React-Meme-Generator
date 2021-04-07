248946101b5c1d25c0c9d7c946e1e6376c5c79d7
Ryan Wiggins <ryanwiggins52@gmail.com>
2021-04-07 09:59:42 -0400

    project finished

--- react-meme-generator/src/MemeGenerator.js
+++ react-meme-generator/src/MemeGenerator.js
@@ -10,6 +10,7 @@ class MemeGenerator extends Component {
             allMemeImgs: []
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount() {
@@ -27,10 +28,17 @@ class MemeGenerator extends Component {
         this.setState({ [name]: value })
     }
 
+    handleSubmit(event) {
+        event.preventDefault()
+        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+        const randMemeImg = this.state.allMemeImgs[randNum].url
+        this.setState({ randomImg: randMemeImg })
+    }
+
     render() {
         return (
             <div>
-                <form className="meme-form">
+                <form className="meme-form" onSubmit={this.handleSubmit}>
                 <input 
                         type="text"
                         name="topText"
